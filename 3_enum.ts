0a3fb77712a0d0b1609850e082bd9786cc246394
gpasha <g1ebikpasha@gmail.com>
2021-08-06 15:59:40 +0300

    enum

---
+++ 3_enum.ts
@@ -0,0 +1,21 @@
+enum Membership{
+    Simple,
+    Standard,
+    Premium
+}
+
+const mmbspSt = Membership.Standard
+const mmbspPr = Membership[2]
+
+console.log(mmbspSt)
+console.log(mmbspPr)
+
+enum SocialMedia{
+    VK = 'VK',
+    FC = 'FACEBOOK',
+    INST = 'INSTAGRAM'
+}
+
+const social = SocialMedia.FC
+
+console.log(social)
